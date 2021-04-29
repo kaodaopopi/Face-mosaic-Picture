f55389a25c0074e3d4b5bae16034285d578cab6c
Kao <66681691+kaodaopopi@users.noreply.github.com>
2021-04-29 18:10:02 +0800

    Add files via upload

--- Face_mosaic(Picture).py
+++ Face_mosaic(Picture).py
@@ -1,24 +1,24 @@
 import cv2
 
-# 載入分類器
+# Load the classifier
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# 讀取圖片
+# load picture
 img = cv2.imread('a.jpg')
 
 
-# 轉成灰階圖片
+# Convert to grayscale image
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# 偵測臉部
+# Detect face
 faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3,minSize=(25, 25))
     
-# 繪製人臉部份的方框
+# Draw the box of the face part
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#(0, 255, 0)欄位可以變更方框顏色(Blue,Green,Red)
+#(0, 255, 0)The field can change the box color(Blue,Green,Red)
 
-#馬賽克
+#Mosaic
 def do_mosaic(faces,x,y,w,h,neighbor = 10):
     fh,fw = img.shape[0],img.shape[1]
     if(y+h>fh)or (x+w>fw):
@@ -33,9 +33,9 @@ def do_mosaic(faces,x,y,w,h,neighbor = 10):
 
 do_mosaic(faces,x, y, w, h)
 
-# 顯示成果
-cv2.namedWindow('img', cv2.WINDOW_NORMAL)  #正常視窗大小
-cv2.imshow('img', img)                     #秀出圖片
-cv2.imwrite( "result.jpg", img )           #保存圖片
-cv2.waitKey(0)                             #等待按下任一按鍵
-cv2.destroyAllWindows()                    #關閉視窗
+# Show results
+cv2.namedWindow('img', cv2.WINDOW_NORMAL)  #Normal window size
+cv2.imshow('img', img)                     #Show pictures
+cv2.imwrite( "result.jpg", img )           #Save Picture
+cv2.waitKey(0)                             #Wait for any key to be pressed
+cv2.destroyAllWindows()                    #Close window
